245862d169aeef961af10d11bd726193b3d68d50
soshope35 <soshope001@gmail.com>
2026-09-05 01:46:37 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+set -e
+
+if [ -z "$XRAY_UUID" ]; then
+    echo "ERROR: XRAY_UUID is not set"
+    exit 1
+fi
+
+cat > /etc/xray/config.json <<EOF
+{
+  "log": {
+    "loglevel": "warning"
+  },
+  "inbounds": [
+    {
+      "listen": "0.0.0.0",
+      "port": 8080,
+      "protocol": "vless",
+      "settings": {
+        "clients": [
+          {
+            "id": "$XRAY_UUID",
+            "level": 0,
+            "email": "user@example.com"
+          }
+        ],
+        "decryption": "none"
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": "/mustapha35"
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    }
+  ]
+}
+EOF
+
+exec /usr/local/bin/xray -config /etc/xray/config.json
